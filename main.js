8854a842a9700271ba6853e2b793f9ff47806662
Brady Kloek <bradywkloek@gmail.com>
2023-12-21 16:20:58 -0600

    Working pupil, dilates with position
    Dilation is instant for idle animations

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const pupil = document.getElementById('pupil');
+const circles = document.getElementsByClassName("circle");
 const w=innerWidth;
 const h=innerHeight;
 const scaleX=200;
 const scaleY=100; 
 const idleTime=2000;
 const idleThreshold=0.2;
+const angle = 2*Math.PI/circles.length;
+const inRad=22;
+const outRad=15;
 
 const followDuration=400;
 const resetDuration=1000;
@@ -14,32 +18,35 @@ const followEase="ease";
 const resetEase="cubic-bezier(0.5,0,0.5,1)"
 const idleEase="cubic-bezier(0.3,0,0.5,1)"
 
-animate(w/2,h/2);
-
 var duration=resetDuration;
 var ease=resetEase;
+var radius=25;
 
 var idle=false;
+var moving=false;
 var idleTimer = setTimeout(1);
 
+var distance;
+const maxDist=Math.sqrt(Math.pow((w/2),2)+Math.pow((h/2),2));
+
+animate(w/2,h/2);
+distributeCircles();
+
 function animate(x,y){
+    distance=Math.sqrt(Math.pow((x-w/2),2)+Math.pow((y-h/2),2));
+    radius=inRad+(outRad-inRad)*(distance/maxDist);
+
     yMult=Math.cos((w/2-x)/960*Math.PI/2.5);
     x=(x-w/2)*scaleX/w+w/2;
     y=(y-h/2)*scaleY*yMult/h+h/2;
 
-    currentX=x;
-    currentY=y;
-    
+
+    console.log(distance+"     "+maxDist);
     pupil.animate({
         left: `${x}px`,
         top: `${y}px`
     },{duration: duration, fill: "forwards", easing: ease});
-}
-
-function calcDuration(x,y){
-    var distance = Math.sqrt(Math.pow((x-currentX),2)+Math.pow((y-currentY),2));
-    
-    return distance/speed;
+    distributeCircles();
 }
 
 document.addEventListener("mousemove", function(e){
@@ -75,7 +82,6 @@ function startIdle() {
 }
 
 function idleRoam(i){
-    console.log(i);
     if(i>0){
         duration=idleDuration;
         ease=idleEase;
@@ -83,4 +89,11 @@ function idleRoam(i){
         idleTimer = setTimeout(()=>{idleRoam(i-1);},random(2000,3500))
     }   
     else startIdle();
+}
+
+function distributeCircles(){
+        for(var i = 0 ; i<circles.length ; i++){
+            circles[i].style.transform = `translate(${radius*Math.cos(i*angle)}px,${radius*Math.sin(i*angle)}px) `
+        }
+    requestAnimationFrame(distributeCircles);
 }
