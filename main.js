45c87bcd4418e56fc2b023ef5bc46c7ea8889671
Brady Kloek <bradywkloek@gmail.com>
2023-12-21 17:36:49 -0600

    Eye fully implemented

--- main.js
+++ main.js
@@ -2,25 +2,29 @@ const pupil = document.getElementById('pupil');
 const circles = document.getElementsByClassName("circle");
 const w=innerWidth;
 const h=innerHeight;
-const scaleX=200;
-const scaleY=100; 
+const scaleX=190;
+const scaleY=80; 
 const idleTime=2000;
 const idleThreshold=0.2;
 const angle = 2*Math.PI/circles.length;
-const inRad=22;
-const outRad=15;
+const followRad=19;
+const idleRad=15;
+const minScale=0.91;
 
 const followDuration=400;
 const resetDuration=1000;
 const idleDuration=1000;
+const dilateDurationScale=0.6;
 
 const followEase="ease";
 const resetEase="cubic-bezier(0.5,0,0.5,1)"
 const idleEase="cubic-bezier(0.3,0,0.5,1)"
 
+const dilateTimeScale=5;
+
 var duration=resetDuration;
 var ease=resetEase;
-var radius=25;
+var radius=idleRad;
 
 var idle=false;
 var moving=false;
@@ -29,27 +33,29 @@ var idleTimer = setTimeout(1);
 var distance;
 const maxDist=Math.sqrt(Math.pow((w/2),2)+Math.pow((h/2),2));
 
-animate(w/2,h/2);
-distributeCircles();
+startIdle();
+
+
 
 function animate(x,y){
     distance=Math.sqrt(Math.pow((x-w/2),2)+Math.pow((y-h/2),2));
-    radius=inRad+(outRad-inRad)*(distance/maxDist);
 
     yMult=Math.cos((w/2-x)/960*Math.PI/2.5);
     x=(x-w/2)*scaleX/w+w/2;
     y=(y-h/2)*scaleY*yMult/h+h/2;
+    
 
 
-    console.log(distance+"     "+maxDist);
     pupil.animate({
         left: `${x}px`,
-        top: `${y}px`
+        top: `${y}px`,
+        scale: `${.99-(1-minScale)*distance/maxDist}`            //.99 to make it so the pupil is never at full scale, solving an issue where the graphic looked slightly different in each state
     },{duration: duration, fill: "forwards", easing: ease});
     distributeCircles();
 }
 
 document.addEventListener("mousemove", function(e){
+    if(idle) dilate(radius,followRad,followDuration*dilateDurationScale);
     idle=false;
     setTimeout(()=>{duration=followDuration; ease=followEase;},500);
     animate(e.clientX, e.clientY);
@@ -76,15 +82,17 @@ function startIdle() {
     duration=resetDuration;
     ease=resetEase;
     animate(w/2,h/2);
+    dilate(radius,idleRad,resetDuration*dilateDurationScale);
 
     let i=Math.floor(random(2,3.5))
-    idleTimer = setTimeout(()=>{idleRoam(i);},random(8000,16000))
+    idleTimer = setTimeout(()=>{idleRoam(i);},random(10000,18000))
 }
 
 function idleRoam(i){
     if(i>0){
         duration=idleDuration;
         ease=idleEase;
+        dilate(radius,followRad,idleDuration*dilateDurationScale);
         animate((0.5+pm()*(random(.2,.5)))*w,(0.5+pm()*(random(.2,.5)))*h)
         idleTimer = setTimeout(()=>{idleRoam(i-1);},random(2000,3500))
     }   
@@ -96,4 +104,17 @@ function distributeCircles(){
             circles[i].style.transform = `translate(${radius*Math.cos(i*angle)}px,${radius*Math.sin(i*angle)}px) `
         }
     requestAnimationFrame(distributeCircles);
+}
+
+function dilate(start, end, time){
+    var change=(end-start)/time*dilateTimeScale;
+    dilateChange(time, change);
+}
+
+function dilateChange(time, change){
+    if(time>0){
+        time-=dilateTimeScale;
+        radius+=change;
+        setTimeout(()=>{dilateChange(time,change)},dilateTimeScale);
+    }
 }
