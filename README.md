8a78af1178804fec840620f7d5a9c48943af586c
Brady Kloek <bradywkloek@gmail.com>
2025-09-07 21:50:40 -0500

    Added README, style fixes

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a Live Wallpaper intended to be used with [Lively Wallpaper](https://apps.microsoft.com/detail/9ntm2qc6qws7?hl=en-US&gl=US) for Windows. It features an animated eye that follows the user's mouse and animates randomly when idle.
+
+![Demonstration Gif](https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeWo3c2J5bmZhMnRkNHdrbTF6b253Z21udzM3MGpsd2t0OXNtbTlyZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fwCmID8mgqrd5PpjLQ/giphy.gif)
+
+## Installation
+1. Download [Lively Wallpaper](https://apps.microsoft.com/detail/9ntm2qc6qws7?hl=en-US&gl=US)
+2. Download this repository
+3. In Lively Wallpaper, click on the "+" icon in the top right to add a new Wallpaper
+4. Navigate to the local copy of this repository in the file explorer popup
+5. Click on `index.html`
+6. Save this as a new Wallpaper
--- main.js
+++ main.js
@@ -4,29 +4,30 @@ const circles = document.getElementsByClassName("circle");
 const width=window.innerWidth;
 const height=window.innerHeight;
 const scaling = window.getComputedStyle(document.body).getPropertyValue('--scaling');
-const scaleX=200*scaling;
-const scaleY=80*scaling; 
-const maxCenterDistance = Math.sqrt(Math.pow((width/2),2)+Math.pow((height/2),2));  // The furthest distance from the center
+const scaleX = 200 * scaling;
+const scaleY = 80 * scaling; 
+const maxCenterDistance = Math.sqrt(Math.pow((width / 2), 2) + Math.pow((height / 2), 2));  // The furthest distance from the center
 
-const movingRadius=19*scaling;
+const movingRadius = 19 * scaling;
 const movingDuration = 1500;
-const movingEase="cubic-bezier(0.3,0,0.5,1)";
+const movingEase = "cubic-bezier(0.3,0,0.5,1)";
 
-const idleRadius=15*scaling;
+const idleRadius = 15 * scaling;
 const idleDuration = 1500;
-const idleEase="cubic-bezier(0.5,0,0.5,1)";
+const idleEase = "cubic-bezier(0.5,0,0.5,1)";
 
-const minScale=0.9;
+const minScale = 0.9;
 
-const dilateStepSize=10;
-var currentRadius = idleRadius;
+const dilateStepSize = 10;
 
-const idleTime=2000;
+let currentRadius = idleRadius;
+
+const idleTime = 2000;
 let idle = true;
 let idleTimeout = setTimeout(0);
 let dilationTimeout = null;
 
-const distributionInterval = 1000/60;   // Circles are distributed at 60fps
+const distributionInterval = 1000 / 60;   // Circles are distributed at 60fps
 let lastDistributionTime = 0;
 
 // Initial Setup: pupil is centered and idling is started
@@ -35,20 +36,19 @@ pupil.style.top = `${height/2}px`;
 distributeCircles();    // This begins an animation loop
 startIdle();
 
-function animatePupil(x,y,duration,easing){
+function animatePupil(x, y, duration, easing){
     // x and y are transformed to coordinates that are within the eye and follow the eye's curvature
-    newX=(x-width/2)*scaleX/width+width/2;
-    newY=(y-height/2)*scaleY*(Math.cos((width/2-x)/960*Math.PI/2.5))/height+height/2;
-    centerDistance = distance(width/2, height/2, x, y);
+    newX = (x - width / 2) * scaleX / width + width / 2;
+    newY = (y - height / 2) * scaleY * (Math.cos((width / 2 - x) / 960 * Math.PI / 2.5)) / height + height / 2;
+    let centerDistance = distance(width / 2, height / 2, x, y);
     pupil.animate({
         left: `${newX}px`,
         top: `${newY}px`,
         // Pupil is scaled down the further it gets from the center
-        scale: `${.99-(1-minScale)*centerDistance/maxCenterDistance}`    
+        scale: `${.99 - (1 - minScale) * centerDistance / maxCenterDistance}`
         /* The max scale is set to .99 to avoid a graphical bug where the max size
          * appears slightly different from any other scale due to pixel alignment. */
-        
-    },{duration: duration, fill: "forwards", easing: easing});
+    }, {duration: duration, fill: "forwards", easing: easing});
 }
 
 function distance(x0,y0,x1,y1){
@@ -58,7 +58,7 @@ function distance(x0,y0,x1,y1){
 document.addEventListener("mousemove", function(e){
     resetTimer();
     if(idle){
-        idle=false;
+        idle = false;
         dilate(movingRadius,200);
     }
     animatePupil(e.clientX, e.clientY, movingDuration, movingEase);
@@ -74,48 +74,48 @@ function random(min,max){
 }
 
 function startIdle() {
-    idle=true;
-    animatePupil(width/2,height/2, idleDuration, idleEase);   // Animate back to center
-    dilate(idleRadius,800);
+    idle = true;
+    animatePupil(width / 2, height / 2, idleDuration, idleEase);   // Animate back to center
+    dilate(idleRadius, 800);
 
-    let numLocations=Math.floor(random(2,3.5)) // Either 2 or 3 locations will be picked, with a greater weighting toward 2
-    idleTimeout = setTimeout(()=>{pupilRoam(numLocations, width/2, height/2);},randomRange(5000, 10000))    // Wait between 5 and 10 seconds before starting to idle 
+    let numLocations = Math.floor(random(2, 3.5)) // Either 2 or 3 locations will be picked, with a greater weighting toward 2
+    idleTimeout = setTimeout(()=>{pupilRoam(numLocations, width / 2, height / 2);}, randomRange(5000, 10000))    // Wait between 5 and 10 seconds before starting to idle 
 }
 
 function plusOrMinus(){
-    if(Math.random()>0.5) return 1;
+    if(Math.random() > 0.5) return 1;
     else return -1;
 }
 
 function randomRange(min,max){
-    return Math.random()*(max-min)+min;
+    return Math.random() * (max - min) + min;
 }
 
 function pupilRoam(numLocations, startX, startY){
-    if(numLocations>0){
-        dilate(movingRadius,500);
+    if(numLocations > 0){
+        dilate(movingRadius, 500);
 
-        newX = randomRange(0,width);
-        newY = randomRange(0,height);
-        centerDistance = distance(startX, startY, newX, newY); 
+        let newX = randomRange(0, width);
+        let newY = randomRange(0, height);
+        let centerDistance = distance(startX, startY, newX, newY); 
         while(centerDistance < maxCenterDistance * 0.2){
-            newX = randomRange(0,width);
-            newY = randomRange(0,height);
+            newX = randomRange(0, width);
+            newY = randomRange(0, height);
             centerDistance = distance(startX, startY, newX, newY); 
         }
-        animatePupil(newX,newY, movingDuration, movingEase)
-        idleTimeout = setTimeout(()=>{pupilRoam(numLocations-1,newX, newY);},random(2000,3500))
+        animatePupil(newX, newY, movingDuration, movingEase)
+        idleTimeout = setTimeout(()=>{pupilRoam(numLocations-1, newX, newY);}, random(2000, 3500))
     }   
     else startIdle();
 }
 
 function distributeCircles(){
-    now = performance.now();
+    let now = performance.now();
     if(now - lastDistributionTime > distributionInterval){
         lastDistributionTime = now;
-        angle = 2*Math.PI/circles.length;
-        for(var i = 0 ; i<circles.length ; i++){
-            circles[i].style.transform = `translate(${currentRadius*Math.cos(i*angle)}px,${currentRadius*Math.sin(i*angle)}px) `
+        let angle = 2 * Math.PI / circles.length;
+        for(let i = 0 ; i<circles.length ; i++){
+            circles[i].style.transform = `translate(${currentRadius * Math.cos(i * angle)}px, ${currentRadius * Math.sin(i * angle)}px) `
         }
     }
     requestAnimationFrame(distributeCircles);
@@ -126,7 +126,7 @@ function dilate(newRadius, time){
         clearTimeout(dilationTimeout);
         dilationTimeout = null;
     }
-    change = dilateStepSize * (newRadius-currentRadius) / time;
+    let change = dilateStepSize * (newRadius-currentRadius) / time;
     dilateChange(newRadius, time, change);
 }
 
